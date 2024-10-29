e04987d363d1da98dbe02543091542b621aab398
Anish Suman <62011810+anish877@users.noreply.github.com>
2024-10-30 01:17:06 +0530

    Add files via upload

---
+++ daily tasks/index.js
@@ -0,0 +1,7 @@
+
+var intTypeVar = 9
+var stringTypeVar = 'Anish'
+var boolTypeVar = true
+var nullTypeVar = null
+var undefinedTypeVar
+console.log( typeof(intTypeVar),'\n',typeof(stringTypeVar),'\n',typeof(boolTypeVar),'\n',typeof(nullTypeVar),'\n',typeof(undefinedTypeVar))
