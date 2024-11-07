a7b59b371763bd899673afdce06dc768c388d754
Anish Suman <62011810+anish877@users.noreply.github.com>
2024-11-08 00:54:29 +0530

    Add files via upload

---
+++ daily tasks/assignmentoperator.js
@@ -0,0 +1,16 @@
+var x=10
+var y=20
+if(x>y){
+    console.log('X IS GREATER  ')
+}
+else{
+    console.log('Y IS GREATER  ')
+}
+if(x>=8){
+    console.log('X IS GREATER THAN 8  ')
+}
+console.log(x===8)
+console.log(x!==5)
+console.log(x>5 && y>10)
+console.log(y>20 || x<5)
+ 
