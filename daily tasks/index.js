0ef5e05752f7d9a886180271ac5b056938508378
Anish Suman <62011810+anish877@users.noreply.github.com>
2024-11-18 21:16:29 +0530

    Update index.js

--- daily tasks/index.js
+++ daily tasks/index.js
@@ -1,7 +1,19 @@
 
-var intTypeVar = 9
-var stringTypeVar = 'Anish'
-var boolTypeVar = true
-var nullTypeVar = null
-var undefinedTypeVar
-console.log( typeof(intTypeVar),'\n',typeof(stringTypeVar),'\n',typeof(boolTypeVar),'\n',typeof(nullTypeVar),'\n',typeof(undefinedTypeVar))
+let integerValue = 9;
+let stringValue = 'Anish';
+let booleanValue = true;
+let nullValue = null;
+let undefinedValue;
+
+function displayType(value, description) {
+    console.log(`${description}: ${typeof value}`);
+}
+
+console.log('--- Data Types ---');
+displayType(integerValue, 'Integer Value');
+displayType(stringValue, 'String Value');
+displayType(booleanValue, 'Boolean Value');
+displayType(nullValue, 'Null Value');
+displayType(undefinedValue, 'Undefined Value');
+
+console.log(`\nNote: In JavaScript, 'null' is considered an ${typeof nullValue}, which is a historical bug.`);
